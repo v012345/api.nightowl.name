e5e68449f0c73856c16980b59e34b8e9a74ea323
Meteor <meteor@192.168.0.101>
2021-09-10 06:50:34 +0800

    test api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    // return view('welcome');
+    return array("code"=>400,"status"=>"OK");
+});
+
