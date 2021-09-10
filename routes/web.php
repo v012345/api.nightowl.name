969b3654224541d6b5ecda0c2f274e9f012f7666
Meteor <v138468@gmail.com>
2021-09-10 02:22:01 -0700

    delete welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return array("code"=>200,"status"=>"OK");
 });
