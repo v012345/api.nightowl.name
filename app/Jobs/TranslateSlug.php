f27c653a7c8ba8c381bcdcb884962108e10f6c95
Meteor <v012345@163.com>
2021-09-17 07:01:40 +0800

    i

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -37,7 +37,7 @@ class TranslateSlug implements ShouldQueue
     {
         //
         $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
-        sleep(20);
+        // sleep(20);
         DB::table('topics')->where("id", $this->topic->id)->update(["slug" => $slug]);
     }
 }
