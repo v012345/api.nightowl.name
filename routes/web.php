a5fd597aeb63679562a2c575386e920d2cf69b46
Meteor <v138468@gmail.com>
2021-09-19 20:52:27 +0800

    i

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (Request $request) {
-    return $request->echostr;
+    return $request->all();
     return view('welcome');
 });
 
