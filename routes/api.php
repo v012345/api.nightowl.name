491f2a56468c41a61a2b7709d1395eca2ca2e01a
Meteor <v138468@gmail.com>
2021-10-14 00:15:19 +0800

    path

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("api2", function (Request $request) {
+    return $request->path();
+});
+Route::get("/", function (Request $request) {
+    return $request->path();
+});
