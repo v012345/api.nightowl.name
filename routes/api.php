7c77b83a31117bed9929650fa7e4ed90311af32d
Meteor <v138468@gmail.com>
2021-10-14 00:22:03 +0800

    i

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("*", function (Request $request) {
+Route::get("{any}", function (Request $request) {
     return $request->path();
-});
+})->where('any', '.*');
