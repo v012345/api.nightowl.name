eadf5570ab07b8047ea45dc26502f9cb52caf61d
Meteor <v138468@gmail.com>
2021-11-13 00:46:01 +0800

    i

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post("hotfix", function (Request $request) {
-    // Log::debug($request);
     $x_hub_signature_256 = $request->header("x-hub-signature-256");
     $signature = "sha256=" . hash_hmac("sha256", $request->getContent(), config("github.secret"));
     if (hash_equals($x_hub_signature_256, $signature)) {
-        //通过验签
-        Log::debug(shell_exec("git -C " . base_path() . " pull origin master"));
+        echo shell_exec("bash " . base_path() . "/scripts/gitpull.sh " . base_path());
     }
 });
-
-Route::get("test", function (Request $request) {
-
-
-
-    echo shell_exec("bash " . base_path() . "/scripts/gitpull.sh " . base_path());
-});
-
-//..
--- scripts/gitpull.sh
+++ scripts/gitpull.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "path:  $1"
 sudo git -C $1 pull origin master
