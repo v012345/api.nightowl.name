ebd9d0656393d0e95cae8510a53d4d8a2d57aaaf
Meteor <v138468@gmail.com>
2022-01-12 23:00:38 +0800

    test

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -10,8 +10,6 @@ class PayController extends Controller
     //
     public function transfer(Request $request)
     {
-        $secret = env("PAY_SECRET");
-        return $request->out_biz_no . $request->trans_amount . $request->payee_info["identity"] . $request->payee_info["name"] . $request->nonce . $secret;
         return Pay::alipay()->transfer([
             'out_biz_no' => $request->out_biz_no,
             'trans_amount' => $request->trans_amount,
