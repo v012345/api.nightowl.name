5615d19d88d57d2fe6743f3bd27112981d088e39
Meteor <v138468@gmail.com>
2022-01-12 19:49:31 +0800

    test

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Yansongda\LaravelPay\Facades\Pay;
 
 class PayController extends Controller
 {
     //
     public function transfer(Request $request)
     {
-        return 232;
+        $result = Pay::alipay()->transfer([
+            'out_biz_no' => "A" + time(),
+            'trans_amount' => '0.01',
+            'product_code' => 'TRANS_ACCOUNT_NO_PWD',
+            'biz_scene' => 'DIRECT_TRANSFER',
+            'payee_info' => [
+                'identity' => '15521224344',
+                'identity_type' => 'ALIPAY_LOGON_ID',
+                'name' => '沙箱环境'
+            ],
+        ]);
+        return $result;
     }
 }
