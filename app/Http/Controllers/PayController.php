658d2d48587605c0989401f3e11fdc23c8fad986
Meteor <v138468@gmail.com>
2022-01-12 21:16:23 +0800

    test

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -10,19 +10,20 @@ class PayController extends Controller
     //
     public function transfer(Request $request)
     {
-
-
-        $result = Pay::alipay()->transfer([
-            'out_biz_no' => time(),
-            'trans_amount' => '1.00',
-            'product_code' => 'TRANS_ACCOUNT_NO_PWD',
-            'biz_scene' => 'DIRECT_TRANSFER',
-            'payee_info' => [
-                'identity' => '15521224344',
-                'identity_type' => 'ALIPAY_LOGON_ID',
-                'name' => '张智铭'
-            ],
-        ]);
-        return $result;
+        $request->out_biz_no;
+        $request->trans_amount;
+        $request->payee_info;
+        // $result = Pay::alipay()->transfer([
+        //     'out_biz_no' => time(),
+        //     'trans_amount' => '1.00',
+        //     'product_code' => 'TRANS_ACCOUNT_NO_PWD',
+        //     'biz_scene' => 'DIRECT_TRANSFER',
+        //     'payee_info' => [
+        //         'identity' => '15521224344',
+        //         'identity_type' => 'ALIPAY_LOGON_ID',
+        //         'name' => '张智铭'
+        //     ],
+        // ]);
+        // return $result;
     }
 }
