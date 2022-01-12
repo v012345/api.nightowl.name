b355324038d450305fca03c1195afb541cf96866
Meteor <v138468@gmail.com>
2022-01-12 21:59:38 +0800

    test

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -28,4 +28,26 @@ class PayController extends Controller
         // ]);
         // return $result;
     }
+
+    public function sign(Request $request)
+    {
+        $request->out_biz_no;
+        $request->nonce;
+        $request->trans_amount;
+        $request->payee_info;
+        $secret = env("PAY_SECRET");
+        return hash("sha256", $request->out_biz_no . $request->nonce . $request->trans_amount . $request->payee_info["identity"] . $secret);
+        // $result = Pay::alipay()->transfer([
+        //     'out_biz_no' => time(),
+        //     'trans_amount' => '1.00',
+        //     'product_code' => 'TRANS_ACCOUNT_NO_PWD',
+        //     'biz_scene' => 'DIRECT_TRANSFER',
+        //     'payee_info' => [
+        //         'identity' => '15521224344',
+        //         'identity_type' => 'ALIPAY_LOGON_ID',
+        //         'name' => '张智铭'
+        //     ],
+        // ]);
+        // return $result;
+    }
 }
